e5a7557ab2c347f49b7affc70e6c7aa631ef4fa3
IanChen0627 <79312556+ian50413@users.noreply.github.com>
2021-07-04 15:51:47 +0800

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+"""
+File: hello_world.py
+Name: Ian Chen
+-----------------------------------
+This program prints 'hello, world!'
+on Console and shows the basic 
+Python syntax
+"""
+
+def main():
+	print("Hello, World!", end="")
+	print('Spider Man', end="")
+	print('Fun Fact: I broke my arm when I was in sixth grade', end="")
+
+### DO NOT EDIT THE CODE BELOW THIS LINE ###
+
+if __name__ == '__main__':
+	main()
